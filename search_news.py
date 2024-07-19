38100fbc93194ce18d5e9645095040e7c223ac95
Amitav Chris Mostafa <a.mostafa@cheil.com>
2024-07-19 21:18:58 +0200

    feat: use params to make api call instead of hard url

--- search_news.py
+++ search_news.py
@@ -10,15 +10,24 @@ def get_api_request(): ...
 def search_news_articles(
     topic: str,
     api_key: str,
+    url: str = "https://newsapi.org/v2/everything",
     language: Optional[str] = None,
+    from_date: Optional[str] = None,
     sort_type: str = "relevancy",
 ):
-    query = f"q={topic}"
-    sort = f"&sortBy={sort_type}"
-    language: Optional[str] = f"&language={language}" if language else None
-    url = f"https://newsapi.org/v2/everything?{query}&from={datetime.now() - timedelta(days=30)}{language}{sort}&apiKey={api_key}"
 
-    response = requests.get(url)
+    params = {
+        "q": topic,
+        "from": from_date if from_date else datetime.now() - timedelta(days=30),
+        "sortBy": sort_type,
+        "language": language,
+        "apiKey": api_key,
+    }
+
+    response = requests.get(
+        url,
+        params=params,
+    )
     articles = []
 
     print(response.status_code)
@@ -28,14 +37,17 @@ def search_news_articles(
             print(
                 "Unauthorized Error: There seems to be something wrong with your API Key"
             )
+            response.raise_for_status()
         case 429:
             print(
                 "Too Many Requests Error: You made too many requests. Take a breather before trying again"
             )
+            response.raise_for_status()
         case 500:
             print(
                 "Server Error: News API is having server issues. There's not quick fix for this :("
             )
+            response.raise_for_status()
         case 200:
             data = response.json()
             # print(data)
@@ -47,6 +59,8 @@ def search_news_articles(
                         "publishedAt": article["publishedAt"],
                     }
                 )
+        case _:
+            response.raise_for_status()
     return articles
 
 
