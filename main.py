34aff2acf6db306438ab2602923e729f969914c8
Amitav Chris Mostafa <a.mostafa@cheil.com>
2024-07-19 07:46:03 +0200

    refactor: Format lines and strings, add dev comments

--- main.py
+++ main.py
@@ -28,33 +28,47 @@ def main():
     # Run Application
     while ApplicationMode.RUN.value:
         topic = input(
-            "What topic would you like to search?\nType 'help' to get more assistance\nType 'exit' or 'quit' to close application\n>>"
+            "What topic would you like to search?\n"\
+            "Type 'help' to get usage instructions\n"\
+            "Type 'exit' or 'quit' to close application\n"\
+            ">>"
         )
+
+        # Guard Clauses to treat input flags
+        # Exit Application
         if topic.strip().lower() == ("exit" or "quit"):
             break
+        # Help String
         if topic.strip().lower() == "help":
-            print(f"Not yet implemented\n")
+            print(f"- Language Setting\n\t-- Use command '!setlang' on prompt to change query language\n"\
+                "- Advanced Queries\n"\
+                "\t--Put your topic in quotation marks for exact match. (eg: \"elon musk\")\n"\
+                "\t--Use '+' and '-' to specify which keywords must or must not appear. (eg: gamestop +stonks -sell)\n"\
+                "\t--Use Boolean Operators. (eg: (crypto AND bitcoin) NOT ethereum)\n"\
+                "\t--Limit search to titles or content . (eg: InTitle=\"title search\")\n")
             continue
+        # Change Language
         if topic.strip().lower() == "!setlang":
             language = (
                 input(
-                    "Restrict your output to a specific language\nType 'en' for English or 'de' for German and press Enter\n>>"
+                    "Restrict your output to a specific language\n"\
+                    "Type 'en' for English or 'de' for German and press Enter\n"\
+                    ">>"
                 )
                 .strip()
                 .lower()
             )
-            print(f"Language is set to {language}\n")
+            print(f"Language is set to >>{language}<<\n")
             continue
 
+        # Search Articles from News API
         print(f"Alright! I will the web for articles about '{topic}'")
-
         spinner = Spinner("Fetching articles...")
         spinner.start()
-
         articles = search_news_articles(topic, NEWSAPI_KEY)
-
         spinner.stop()
 
+        # If no articles found, let us know some details and skip iteration
         if not articles:
             print(f"No articles found for the topic\n>>{topic}<<\nin >>{language}<<.\n")
             continue
@@ -74,3 +88,4 @@ def main():
 if __name__ == "__main__":
     print("Starting Application...")
     main()
+    print("Application closed.")
