f89e80d68d7783e025fa30ec2fa553615e7d68ac
Amitav Chris Mostafa <a.mostafa@cheil.com>
2024-07-20 02:07:11 +0200

    feat: Add Summarizer fcns

--- search_news.py
+++ search_news.py
@@ -3,6 +3,12 @@ import requests
 from datetime import datetime, timedelta
 from typing import Optional
 
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import nltk
+from collections import Counter
+
 
 def get_api_request(): ...
 
@@ -64,4 +70,41 @@ def search_news_articles(
     return articles
 
 
-def summarize_content(content): ...
+# Define a Prompt Template for Summary
+summarization_template = PromptTemplate(
+    input_variables=["content"],
+    template="Make a succint summary of the following content: \n{content}\nSummary:",
+)
+
+nltk.download("punkt")
+nltk.download("averaged_perceptron_tagger")
+nltk.download("maxent_ne_chunker")
+nltk.download("words")
+
+
+def summarize_content(content):
+    hf_llm = HuggingFaceEndpoint(model_name="facebook/bart-large-cnn")
+    summarize_chain = LLMChain(llm=hf_llm, prompt=summarize_content)
+    summary = summarize_chain.run({"content": content})
+    return summary
+
+
+def extract_named_entities(content):
+    sentences = nltk.sent_tokenize(content)
+    tokens = [nltk.word_tokenize(sentence) for sentence in sentences]
+    pos_tags = [nltk.pos_tag(token) for token in tokens]
+    chunks = [nltk.ne_chunk(tag) for tag in pos_tags]
+
+    named_entities = []
+    for chunk in chunks:
+        current_chunk = []
+        for tree in chunk:
+            if hasattr(tree, "label"):
+                current_chunk.append(" ".join([child[0] for child in tree]))
+            elif current_chunk:
+                named_entities.append(" ".join(current_chunk))
+                current_chunk = []
+        if current_chunk:
+            named_entities.append(" ".join(current_chunk))
+
+    return Counter(named_entities)
